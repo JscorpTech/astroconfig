9e21a76ba7a2806a525ad198df9c8fedd29de631
A'zamov Samandar <admin@jscorp.uz>
2025-07-17 15:18:44 +0500

    multi cursor updated

--- lua/plugins/multi-cursor.lua
+++ lua/plugins/multi-cursor.lua
@@ -1,8 +1,13 @@
-
-
 return {
   {
     "mg979/vim-visual-multi",
     branch="master",
+    init = function()
+      vim.g.VM_mouse_mappings = 1
+      vim.g.VM_maps = {
+        ["Add Cursor Down"] = "<C-j>",
+        ["Add Cursor Up"] = "<C-k>",
+      }
+  end,
   }
 }
